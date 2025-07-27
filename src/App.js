4246b2df80c3a72ab2e4eda442295939d8a17e87
Nazar Stepaniuk <annastepaniuk77@gmail.com>
2025-07-27 12:02:31 +0200

    added basic structure and navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/home.jsx";
+import Cart from "./pages/cart.jsx";
+import Header from './components/header/index.jsx';
 
 function App() {
     return (
-        <div className="App">
-
+        <div>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/cart' element={<Cart/>}/>
+            </Routes>
         </div>
     );
 }
