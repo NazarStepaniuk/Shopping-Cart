8964c54c13c8a341b27fb8e3b65dcad17033a871
Nazar Stepaniuk <annastepaniuk77@gmail.com>
2025-07-29 19:04:20 +0200

    feat: add addToCart and removeFromCart functionality

--- src/components/product-item/index.jsx
+++ src/components/product-item/index.jsx
@@ -1,11 +1,18 @@
-import {useDispatch} from "react-redux";
-import {addToCart} from "../../store/slices/cart-slice";
+import {useDispatch, useSelector} from "react-redux";
+import {addToCart, removeFromCart} from "../../store/slices/cart-slice";
 
 export default function ProductItem({product}){
+    const {cart} = useSelector(state => state);
+
     const dispatch = useDispatch();
+
     function handleAddToCart(){
         dispatch(addToCart(product));
     }
+    function handleRemoveFromCart(){
+        dispatch(removeFromCart(product.id));
+    }
+
 
     return <div>
         <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
@@ -16,7 +23,12 @@ export default function ProductItem({product}){
                 <h2 className="w-40 truncate mt-3 text-gray-700 font-bold text-lg">{product.title}</h2>
             </div>
             <div className="flex items-center justify-center w-full mt-5">
-                <button onClick={handleAddToCart} className="bg-red-950 text-white border-2 rounded-lg font-bold p-4">Add to cart</button>
+                <button onClick={cart.some(item => item.id === product.id) ? handleRemoveFromCart : handleAddToCart} 
+                        className="bg-red-950 text-white border-2 rounded-lg font-bold p-4">
+                            {
+                                cart.some(item => item.id === product.id) ? 'Remove from cart' : 'Add to cart'
+                            }
+                </button>
             </div>
         </div>
     </div>
